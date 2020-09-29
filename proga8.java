69c867ff085576a2fd5403ef0231ea108af62119
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:35:51 +0300

    Create proga8.java

---
+++ proga8.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class JOJO8 {
+    public static void main(String[] args) {
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        return (centimetre / 100);
+    }
+}
