ccc1f5bba7d8f34252b6590d9b163b66c53984a6
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:42:32 +0300

    Create proga15.java

---
+++ proga15.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class JOJO15 {
+    public static int number = 25;
+    public static void main(String[] args) {
+        number = number * number;
+        System.out.println(number);
+    }
+}
