60129161ad2b420ece91d3b41a49f53dcc015a9b
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:34:20 +0300

    Create proga6.java

---
+++ proga6.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class JOJO6 {
+    public static void main(String[] args) {
+        printCircleLength(5);
+    }
+
+    public static void printCircleLength(int radius) {
+        double pi=3.14;
+        System.out.println(2*pi*radius);
+    }
+}
