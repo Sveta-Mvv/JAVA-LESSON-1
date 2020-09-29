eda157690876b1b00a2526e834b50883a18d9f3d
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:41:36 +0300

    Create proga13.java

---
+++ proga13.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class JOJO13 {
+
+    public static void main(String[] args) {
+        System.out.println(getWeight(888));
+    }
+
+    public static double getWeight(int earthWeight) {
+        return (earthWeight / 100.0 * 17.0);
+    }
+}
