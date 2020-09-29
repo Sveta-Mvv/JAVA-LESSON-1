d463978500b3cb17900045cce906c55acab61bc5
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:32:33 +0300

    Create proga3.java

---
+++ proga3.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class JOJO3 {
+    public static void main(String[] args) {
+        int matuci = 2020-99;
+        String GOD="Юный друг. Сейчас 2020 год. Мтуси был основан 99 лет назад. То есть в "  + matuci;
+        System.out.println(GOD);
+    }
+}
