e31a47255f6bc36b37331457a5653c70cedff86e
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:35:21 +0300

    Create proga7.java

---
+++ proga7.java
@@ -0,0 +1,10 @@
+package com.company;
+public class JOJO7 {
+    public static void main(String[] args) {
+        System.out.println(convertCelsiusToFahrenheit(41));
+    }
+
+    public static float convertCelsiusToFahrenheit(int celsius) {
+        return (9.0f / 5.0f) * celsius + 32;
+    }
+}
