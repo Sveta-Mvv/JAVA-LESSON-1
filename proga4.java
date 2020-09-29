79fd51e8184a82997ebb333559e1038b98784eef
Sveta-Mvv <55664340+Sveta-Mvv@users.noreply.github.com>
2020-09-29 22:33:11 +0300

    Create proga4.java

---
+++ proga4.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class JOJO4 {
+    public static void main(String[] args) {
+        int x = 27;
+        int y = 15;
+        // y = x - y;
+        // y = y - x;
+        // y = y + x;
+        // y = y + x;
+        y = x / y;
+        // y = y / x;
+        // y = y * x;
+        x = x - y;
+        y = y - x;
+        System.out.println(Math.abs(x));
+        System.out.println(Math.abs(y));
+    }
+}
